2a91f3ca6d154528c88bfa3b4ab902768126ad4d
FelixRodriguezJr <79116375+FelixRodriguezJr@users.noreply.github.com>
2023-02-24 02:55:45 -0500

    added git ignore

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
+    TOKEN = os.environ('TOKEN')
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
